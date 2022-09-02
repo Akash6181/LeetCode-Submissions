5f202c3c908b3eafb1f3c141e8bddf2fe4c426e7
Akash Gowda K R <108926049+Akash6181@users.noreply.github.com>
2022-09-02 12:01:08 +0530

    Time: 21 ms (59.49%), Space: 41 MB (41.32%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -6,11 +6,14 @@ public class Solution extends VersionControl {
         if(n==1) return n;
         int low=1;
         int high=n;
+        int index=0;
         while(low<=high){
             int mid=low+(high-low)/2;
             if(isBadVersion(mid)) high=mid-1;
             else low=mid+1;
+            index=mid;
         }
-        return low;
+        if(!isBadVersion(index)) return index+1;
+        else return index;
     }
 }
